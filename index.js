03ebc914744d197361ba11a1e3dcaadbe75ddfd7
wojtas7p <wojtas7p@gmail.com>
2024-06-21 23:17:29 +0200

    Add specific CORS configuration

--- index.js
+++ index.js
@@ -1,31 +1,31 @@
 const express = require('express');
-const cors = require('cors'); // Dodaj import modułu cors
+const cors = require('cors');
 
 const app = express();
-const {port} = require('./config')
+const { port } = require('./config');
 const apiRouter = require('./routes/api');
 
 // Dodaj obsługę CORS
 app.use(cors({
-    origin: 'https://main--akpdatabase.netlify.app' // Podaj adres swojego frontend na Netlify
+    origin: ['https://main--akpdatabase.netlify.app', 'https://akpdatabase.netlify.app'],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+    optionsSuccessStatus: 204
 }));
 
 // Dodaj obsługę parsowania JSON
 app.use(express.json());
 
-//db
-require('./db/mongoose')
+// db
+require('./db/mongoose');
 
 // import note.js to ensure the note is saved
 require('./db/models/note');
 
-// //router
+// router
 app.use('/', apiRouter);
 
-//server
+// server
 app.listen(port, function () {
-    console.log(`Serwer słucha... http://localhost:${port}`);
+    console.log('serwer słucha... http://localhost:' + port);
 });
-
-
-
