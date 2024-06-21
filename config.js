2259acf6ecb81050a0821e67ee7860c78beadce7
wojtas7p <wojtas7p@gmail.com>
2024-06-22 01:27:26 +0200

    Add specific CORS configuration

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    port: process.env.PORT || 4000,
+    port: process.env.PORT || 10000,
     // database: process.env.DATABASE || 'mongodb://127.0.0.1:27017/akpApp'
     database: process.env.DATABASE || 'mongodb+srv://Wojtas7p:NINJApanek7@cluster0.y1cvbzn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
-};
+};                                     
--- index.js
+++ index.js
@@ -1,31 +1,30 @@
 const express = require('express');
-const cors = require('cors');
+const cors = require('cors'); // Dodaj import modułu cors
 
 const app = express();
-const { port } = require('./config');
+const {port} = require('./config')
 const apiRouter = require('./routes/api');
 
 // Dodaj obsługę CORS
-app.use(cors({
-    origin: ['https://main--akpdatabase.netlify.app', 'https://akpdatabase.netlify.app'],
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: true,
-    optionsSuccessStatus: 204
-}));
+app.use(cors());
 
 // Dodaj obsługę parsowania JSON
 app.use(express.json());
 
-// db
-require('./db/mongoose');
+//db
+require('./db/mongoose')
 
 // import note.js to ensure the note is saved
 require('./db/models/note');
 
-// router
+// //router
 app.use('/', apiRouter);
 
-// server
-app.listen(port, function () {
+//server
+app.listen(port, function(){
     console.log('serwer słucha... http://localhost:' + port);
 });
+
+
+
+
