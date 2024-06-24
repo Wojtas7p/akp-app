f9358689a1c1583194b5ebf08ea498ab06b37fd7
wojtas7p <wojtas7p@gmail.com>
2024-06-24 11:25:03 +0200

    Prepare for deployment

--- db/mongoose.js
+++ db/mongoose.js
@@ -6,7 +6,11 @@
 const mongoose = require('mongoose');
 const { database } = require('../config');
 
-mongoose.connect(database, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(database, { useNewUrlParser: true, useUnifiedTopology: true,
+  serverSelectionTimeoutMS: 30000, // Zwiększenie czasu timeout dla wyboru serwera
+  socketTimeoutMS: 45000 // Zwiększenie czasu timeout dla socketu
+})
+
   .then(() => console.log('Connected to MongoDB Atlas'))
   .catch(err => console.error('Could not connect to MongoDB Atlas...', err));
 
