598ac4871103aaf14cd28dfe38ccf789fe47ef37
wojtas7p <wojtas7p@gmail.com>
2024-06-24 12:12:23 +0200

    Prepare for deployment

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
     port: process.env.PORT || 10000,
     // database: process.env.DATABASE || 'mongodb://127.0.0.1:27017/akpApp'
-    database: process.env.DATABASE || 'mongodb+srv://Wojtas7p:NINJApanek7@cluster0.y1cvbzn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+    database: process.env.DATABASE || 'mongodb+srv://wojtas7:ninja7@cluster0.y1cvbzn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
 };                                     
